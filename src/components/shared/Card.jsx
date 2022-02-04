4542fe30221502bd168a84b15632fe2a199189e4
Alex <alexsalazarny@gmail.com>
2022-02-03 21:50:40 -0500

    Added card cmomponent

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+//the purpose of this component is so that whenever we need to show a card 
+//we can pass in the data that will make the card here
+
 //the children that are being passed is the data contained within feedback item, setting up children means 
 //the children are the feedbackitems which are some divs and the items 
 function Card({children}) {
     
-  return <div>{children}</div>;
+  return <div className="card">{children}</div>;
 }
 
 export default Card;
