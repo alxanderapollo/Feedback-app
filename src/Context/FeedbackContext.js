0ee22e1f7ad6539c9fb0b63bc296a8c48f144ccd
Alex <alexsalazarny@gmail.com>
2022-02-09 23:30:45 -0500

    Refactored code

--- src/Context/FeedbackContext.js
+++ src/Context/FeedbackContext.js
@@ -7,25 +7,39 @@ const FeedbackContext = createContext();
 
 //the children that are passed in are all of the compoenets that will be passed in
 export const FeedbackProvider = ({ children }) => {
-    // state to contain the objects with the information will be using
-    //in this case the object represents the id, text and rating`
+  // state to contain the objects with the information will be using
+  //in this case the object represents the id, text and rating`
   const [feedback, setFeedback] = useState([
     {
+      id: 0,
+      text: "This is item1",
+      rating: 1,
+    },
+    {
       id: 1,
-      text: 'This is fromt he context',
-      rating: 10
+      text: "This is item2",
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: "This is item3",
+      rating: 5,
     },
   ]);
 
-    //this process is known as prop drilling, passing up props
-    const deleteFeedback = (id) => {
-        console.log("App", id);
-        //returns an array minus the one we are deleting
-        if (window.confirm("Are you sure you want to delete?"))
-          setFeedback(feedback.filter((item) => item.id !== id));
-      }
-      
-        //checking to see if we are getting feed back added up into the app component
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+  //this process is known as prop drilling, passing up props
+  const deleteFeedback = (id) => {
+    console.log("App", id);
+    //returns an array minus the one we are deleting
+    if (window.confirm("Are you sure you want to delete?"))
+      setFeedback(feedback.filter((item) => item.id !== id));
+  };
+
+  //checking to see if we are getting feed back added up into the app component
   const addFeedback = (newFeedback) => {
     //calling uuid to generate a unique id for us
     newFeedback.id = uuidv4();
@@ -36,12 +50,26 @@ export const FeedbackProvider = ({ children }) => {
     // a New state
     //new feedback is our most our current feedback item, that will be added to the feedback state
     setFeedback([newFeedback, ...feedback]);
-  };;
-    
+  };
+  //item is the particular item we are editing
+  //edit is s state that will change depending on whether we are editing or not
+
+  //set edit item to be updated
+  const editFeedback = (item) => {
+    //set the state for edit item here
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
 
   return (
     //   anything that we need passed down in the components happen here, data or functions....
-    <FeedbackContext.Provider value={{feedback,deleteFeedback, addFeedback }}>{children}</FeedbackContext.Provider>
+    <FeedbackContext.Provider
+      value={{ feedback, deleteFeedback, addFeedback, editFeedback }}
+    >
+      {children}
+    </FeedbackContext.Provider>
   );
 };
 
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useContext } from "react";
-//imports font awesome icons
-import { FaTimes } from "react-icons/fa";
+//imports font awesome icons, FaTimes is the x icon we are using on the cards, FaEdit is the pencil icon
+import { FaTimes, FaEdit } from "react-icons/fa";
+
 import FeedbackContext from "../Context/FeedbackContext";
 
 //recives an item from FeedbackList and appropoiately display each item
 import Card from "./shared/Card";
 export const FeedbackItem = ({ item }) => {
   //pull out delete feed back function
-  const { deleteFeedback } = useContext(FeedbackContext);
+  const { deleteFeedback,editFeedback } = useContext(FeedbackContext);
 
   // card is styled component that can be used wherever we want to display a card
   return (
@@ -20,6 +21,10 @@ export const FeedbackItem = ({ item }) => {
       {/* creates x on the right side of the screen on each card */}
       <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes className="purple" />
+      </button>
+
+      <button className="edit" onClick={() => editFeedback(item)}>
+        <FaEdit color="purple"/>
       </button>
       {/* tgext that goes into the card */}
       <div className="text-display">{item.text}</div>
