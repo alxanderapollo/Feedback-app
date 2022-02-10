c647aab2334e5b782da6989a61121d77d90f7435
Alex <alexsalazarny@gmail.com>
2022-02-10 13:40:17 -0500

    ADDED spinner component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
-import {useContext} from 'react'
+import { useContext } from "react";
 //use context hook so we can use the context in this component
-import FeedbackContext from '../Context/FeedbackContext'
-
+import FeedbackContext from "../Context/FeedbackContext";
+import Spinner from './shared/Spinner'
 import { motion, AnimatePresence } from "framer-motion";
 const FeedbackList = () => {
+  //the hook takes  FeedbackContext
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  //the hook takes FeedbackContext
-  const {feedback} = useContext(FeedbackContext)
+  //if there arent any spinners loading, and the feedback is empty show  no feedback yet
+  if (!isLoading && (!feedback || feedback.length === 0))
+    return <p>No Feedback yet</p>;
 
-
-  if (!feedback || feedback.length === 0) return <p>No Feedback yet</p>;
-
-  return (
+  return isLoading ? (
+    <Spinner/>
+  ) : (
     <div className="feedback-list">
       {/* initlia is opacity,meaning whether the component will be invisible or not */}
       {/* animate it to be visible  */}
@@ -26,10 +28,7 @@ const FeedbackList = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import spinner from'../assests/spinner.gif'
+function Spinner() {
+  return <img src={spinner} alt='Loading...' style={{width: '100px',margin:'auto',display: 'block'}}/>
+}
+
+export default Spinner;
